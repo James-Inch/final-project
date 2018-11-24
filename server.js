76b78a6e342da5c030c2b4f7175924deb7a05580
james inch <james.inch0103@gmail.com>
2018-11-24 14:36:08 -0500

    first scrape

--- server.js
+++ server.js
@@ -32,8 +32,40 @@ mongoose.connect("mongodb://localhost/finalProject", { useNewUrlParser: true });
 // Routes
 
 
+axios.get("https://www.climbing.com/").then(function (response) {
+
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    // console.log(response.data);
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // With cheerio, find each p-tag with the "title" class
+    // (i: iterator. element: the current element)
+    $("div.l-grid--item").each(function (i, element) {
+
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+        var link = $(element).children().attr("href");
+
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+            title: title,
+            link: link
+        });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
+});
+
+
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
-  
+});
