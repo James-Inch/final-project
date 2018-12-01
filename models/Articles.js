fcab1f356f872fb8bb1c6424d53b2701f1153972
james inch <james.inch0103@gmail.com>
2018-12-01 13:30:26 -0500

    add api routes

---
+++ models/Articles.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    picture: {
+        type: String,
+        required: false
+    },
+    title: {
+        type: String, 
+        required: true 
+    }, 
+    link: {
+        type: String, 
+        required: true
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
