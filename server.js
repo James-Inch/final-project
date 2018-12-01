c6b214a28dad53b98ec8dd9397d74c72df58e650
james inch <james.inch0103@gmail.com>
2018-11-30 21:02:00 -0500

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
