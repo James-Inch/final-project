c173ab23754938a74ba34eedf6212814bd4ef425
james inch <james.inch0103@gmail.com>
2018-12-07 18:24:02 -0500

    restart 2

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const routes = require("./routes");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-// const db = require("./models");
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger("dev"));
-app.use(routes);
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
+// Define API routes here
 
-const MONGOD_URI = process.env.MONGOD_URI || "mongodb://localhost/climbMongo"; 
-mongoose.connect(MONGOD_URI, { useNewUrlParser: true });
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
