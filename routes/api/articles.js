17434ad32e2fbf1fdf3b3f12ce17fbd7191245f9
james inch <james.inch0103@gmail.com>
2018-12-12 19:22:55 -0500

    rm some unused code

--- routes/api/articles.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const articleController = require("../../controllers/articleController");
-
-router.route("/").get(articleController.findAll);
-
-module.exports = router;
