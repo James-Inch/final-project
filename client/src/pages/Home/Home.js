030e145700c5523bd14eda8a73db33f45b76243c
james inch <james.inch0103@gmail.com>
2018-12-14 22:20:21 -0500

    revert to scrapedContainer

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import Banner from "../../components/Banner";
+// import Banner from "../../components/Banner";
+import ScrapedContainer from "../../components/ScrapedContainer";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Banner />
+                <ScrapedContainer />
             </div>
                        
         );
